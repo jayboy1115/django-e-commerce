2855795117fcc845ccf476097d523749e7aa5994
johnson <ibitoyejohnson234@gmail.com>
2025-03-10 13:26:19 +0100

    My Project

--- core/models.py
+++ core/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    city = models.CharField(max_lenght=100, blank=True, null=True)
-    state = models.CharField(max_lenght=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
-    phone = models.CharField(max_lenght=15, blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
 
-    def__str__(self):
+    def __str__(self):
         return self.username
 
 # Create your models here.
