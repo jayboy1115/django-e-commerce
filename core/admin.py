b391e71afcfdd3bcee288e3237b9955520817d06
johnson <ibitoyejohnson234@gmail.com>
2025-03-10 11:47:41 +0100

    My Project

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields':('username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'city', 'state',)}
+
+        ),
+    )
+
+admin.site.register(CustomUser, )    
--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    city = models.CharField(max_lenght=100, blank=True, null=True)
+    state = models.CharField(max_lenght=100, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    phone = models.CharField(max_lenght=15, blank=True, null=True)
+
+    def__str__(self):
+        return self.username
 
 # Create your models here.
